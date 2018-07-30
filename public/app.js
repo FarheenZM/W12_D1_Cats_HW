2228208f5804202158b49c6731a8cfa8c0649831
Farheen <farheenzm@gmail.com>
2018-07-30 18:38:43 +0100

    Removed cats from html and added to js file

--- public/app.js
+++ public/app.js
@@ -28,7 +28,10 @@
 var CATS_ARRAY = [
   {name: "Chef Kittie", favFood: "Fish", img: "images/cat1.jpg"},
   {name: "The Twins", favFood: "Whiskas", img: "images/cat2.jpg"},
-  {name: "Baby Blue", favFood: "Milk", img: "images/cat3.jpg"}
+  {name: "Baby Blue", favFood: "Milk", img: "images/cat3.jpg"},
+  {name: "Boba", favFood: "Sock Fluff", img: "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"},
+  {name: "Barnaby", favFood: "Tuna", img: "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"},
+  {name: "Max", favFood: "Whiskas Temptations", img: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"}
 ];
 
 var addCat = function(name, favFood, image){
